96039cc7f6dce0c3c859c8964b7478bd4c8d8ede
Berk Canpolat <canpolatberk19@gmail.com>
2024-12-05 14:15:40 +0300

    Module package CustomCacheNetworkImage added

---
+++ module/common/lib/src/package/custom_image/custom_mem_cache.dart
@@ -0,0 +1,9 @@
+final class CustomMemCache {
+  const CustomMemCache({required this.height, required this.width});
+
+  /// Project Cached Network Image Height
+  final int height;
+
+  /// Project Cached Network Image Width
+  final int width;
+}
